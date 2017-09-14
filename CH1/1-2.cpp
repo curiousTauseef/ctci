7c09da5437332adae2a7a01997064a45e0a175ea
dittoPDX <steventran325@gmail.com>
2017-09-14 14:32:05 -0700

    Updated questions done

---
+++ CH1/1-2.cpp
@@ -0,0 +1,29 @@
+/*
+1.2
+Check Permutation: Given 2 strings, write a method to decide if one is a permutation of the other.
+
+Hints:
+
+#1:
+
+#84:
+
+#122:
+
+#131:
+
+*/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	//Test strings
+	char array1[4] = "cat";
+	char array2[4] = "tac";
+
+	return 0;
+}
