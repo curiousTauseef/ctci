047328ff413515f227a782fb636a2e9be31ee3bf
dittoPDX <steventran325@gmail.com>
2017-09-14 12:41:36 -0700

    Question details

--- CH1/1-1.cpp
+++ CH1/1-1.cpp
@@ -1,4 +1,20 @@
+/*
+1.1
+Is Unique: Implement an algorithm to determine if a string has all unique characters.
+	   What if you cannot use additional data structures?
+
+Hints: #44, #117, #132
+
+44: Try a hash table
+
+117: Could a bit vector be useful?
+
+132: Can you solve it in O(n * log(n)) time? What might a solution like that look like?
+
+*/
+
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
