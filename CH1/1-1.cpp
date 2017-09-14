04e89dd504b7af2db3dff7175b814479850bae04
dittoPDX <steventran325@gmail.com>
2017-09-14 14:50:36 -0700

    Included optimized solution

--- CH1/1-1.cpp
+++ CH1/1-1.cpp
@@ -18,11 +18,13 @@ My Thoughts: First use quicksort -> O(log(n)). Second use a modified linear sear
 
 #include <iostream>
 #include <cstring>
+#include <bitset>
 
 using namespace std;
 
 bool bruteForce(char array[], int size);
 bool optimized(char array[], int size);
+bool optimized2(char array[], int size);
 
 //True for unique, false for not unique
 /*
@@ -81,6 +83,38 @@ bool optimized(char array[], int size)
 	return true;
 }
 
+//True for unique, false for not unique
+/*
+Note: This solution is an improved version of the above solution.
+Time Complexity: O(n)
+Space Complexity: O(1) (improved by factor of 8)
+
+- Explanation -
+TC: The loop iterates through the input "N" times
+SC: Improved by a factor of 8 because a bit table uses 1 bit instead of 8 bits (previous solution)
+*/
+bool optimized2(char array[], int size)
+{
+	if (size <= 1) return true;
+
+	bitset<256> table;
+
+	for (int i = 0; i < size; ++i)
+	{
+		int decOfAscii = array[i];
+
+		if (table[decOfAscii] == true)
+		{
+			return false;
+		}
+
+		table[decOfAscii] = true;	
+	}
+
+	return true;	
+}
+
+
 int main()
 {
 	//Test strings
@@ -92,9 +126,11 @@ int main()
 	//Brute-force solution and optimized solution
 	bool checkIfUnique = bruteForce(nameTwo, 6);
 	bool optimizedCIU = optimized(nameTwo, 6);
+	bool optimized2CIU = optimized2(nameTwo, 6);
 
 	cout << "Unique: " << checkIfUnique << endl;
 	cout << "Unique (o): " << optimizedCIU << endl;	
+	cout << "Unique (o.2): " << optimized2CIU << endl;
 
 	return 0;
 }
