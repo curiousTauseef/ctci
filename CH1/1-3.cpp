655aa6f3a48f0ee8194b5df2729e9fb98f87d9f3
dittoPDX <steventran325@gmail.com>
2017-09-15 00:33:02 -0700

    file additions

--- CH1/1-3.cpp
+++ CH1/1-3.cpp
@@ -1,7 +1,7 @@
 /*
 URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string
 has sufficient space at the end to hold the additional characters, and that you are given the "true"
-length of the string. The algorithm must be in-place.
+length of the string. Try to do this algorithm in-place.
 
 EXAMPLE
 Input:  "Mr John Smith    ", 13
@@ -9,6 +9,8 @@ Output: "Mr%20John%20Smith"
 
 HINTS
 #53: It's often easiest to modify strings by going from the end of the string to the beginning
+Filling in the contents of the "true array" starting at the extra space to the front is the
+most efficient algorithm.
 
 #118: You might find out that you need to know the number of spaces. Can you just count them?
 */
