ffb87f46713c1bea87906ec2febc0479008d231e
dittoPDX <steventran325@gmail.com>
2017-09-15 15:37:22 -0700

    Question info

--- CH1/1-4.cpp
+++ CH1/1-4.cpp
@@ -1,9 +1,34 @@
+/*
+- Palindrome Permutation - 
+Given a string, write a function to check if it is a permutation of a palindrome.
+A palindrome is a word or phrase that is the same forwards & backwards.
+A permutation is a rearrangement of letters. The palindrome does not need to be
+limited to just dictionary words. (ASCII - 128 or 256 for extended)
+
+Hints:
+
+#106:
+
+#121:
+
+#134:
+
+#136:
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+	//Input string
+	char input[9] = "Tact Coa";
+
+	/*
+	Expected output
+	True (permutation: "taco cat", "atco cta", etc.)
+	*/
 
 	return 0;
 }
