59413ce582b6da7405db345ef72246976577d22d
dittoPDX <steventran325@gmail.com>
2017-09-14 23:55:20 -0700

    Added question 1-3

---
+++ CH1/1-3.cpp
@@ -0,0 +1,25 @@
+/*
+URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string
+has sufficient space at the end to hold the additional characters, and that you are given the "true"
+length of the string.
+
+EXAMPLE
+Input:  "Mr John Smith    ", 13
+Output: "Mr%20John%20Smith"
+
+HINTS
+#53: It's often easiest to modify strings by going from the end of the string to the beginning
+
+#118: You might find out that you need to know the number of spaces. Can you just count them?
+*/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
