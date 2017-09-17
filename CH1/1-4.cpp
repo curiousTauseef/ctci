4e7d2693f267a9e9eea3fdd11dc898d1ef2c44ee
dittoPDX <steventran325@gmail.com>
2017-09-16 21:56:03 -0700

    Brute-force solution completed

--- CH1/1-4.cpp
+++ CH1/1-4.cpp
@@ -51,21 +51,75 @@ bool bruteForce(char input[])
 			++trueLen;	
 		}
 	}	
+
+	int oddCount = 0;
+
+	for (int p = 0; p < 127; ++p)
+	{
+		if (trueLen % 2 == 0)
+		{
+			if (table[p] % 2 != 0)
+			{
+				return false;
+			}
+		}
+		else
+		{
+			if (table[p] % 2 != 0)
+			{
+				++oddCount;
+			}
+
+			if (oddCount > 1) return false;
+		}
+	}
+/*
+	if (trueLen % 2 == 0)
+	{
+		for (int j = 0; j < 127; ++j)
+		{
+			if (table[j] % 2 != 0)
+			{
+				return false;
+			}
+		}
+	}
+	else
+	{
+		for (int k = 0; k < 127; ++k)
+		{
+			if (table[k] % 2 != 0)
+			{
+				++oddCount;
+			}
+			
+			if (oddCount > 1)
+			{
+				return false;
+			}
+		}
+	}
+*/
+	return true;
 }
 
 int main()
 {
 	//Input string
 	char input[9] = "tact coa";
+	char input2[7] = "annasp";
 
 	//Boundary-case
 	char test1[1] = "";
 
-	bool checkIfPOP = bruteForce(input);
-	
+	bool checkIfPOP = bruteForce(input2);
+
+	cout << "Permutation of String: " << checkIfPOP << endl;	
+
 	/*
 	Expected output
-	True (permutation: "taco cat", "atco cta", etc.)
+	True 
+	(permutation: "taco cat", "atco cta", etc.)
 	*/
 
 	return 0;
