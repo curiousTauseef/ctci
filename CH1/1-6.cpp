a858eda03c93c06b9fe345b99d0fd3535255ba2b
dittoPDX <steventran325@gmail.com>
2017-09-17 00:50:27 -0700

    Updated question info

--- CH1/1-6.cpp
+++ CH1/1-6.cpp
@@ -1,9 +1,31 @@
+/*
+- String Compression -
+Implement a method to perform basic string compression using the counts
+of repeated characters. 
+
+For example, the string "aabcccccaaa" would become "a2b1c5a3". If the
+"compressed" string would not become smaller than the original string,
+your method should return the original string.
+
+You can assume the strnig has only uppercase and lowercase letters
+(a -z).
+
+- Hints -
+
+#92: Do the easy thing first: compress the string, then compare lengths.
+
+#110: Be careful that you aren't repeatedly concatenating strings
+together. This can be very inefficient.
+
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+	char str1[12] = "aabcccccaaa";
 
 	return 0;
 }
