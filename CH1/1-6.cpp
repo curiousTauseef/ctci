7e7ec7e56fb1aa75bac00be552f5044363bc2894
dittoPDX <steventran325@gmail.com>
2017-09-18 14:43:56 -0700

    Completed brute-force solution w/ <string>

--- CH1/1-6.cpp
+++ CH1/1-6.cpp
@@ -27,30 +27,91 @@ together. This can be very inefficient.
 
 #include <iostream>
 #include <string>
+#include <cstdio>
 #include <cctype>
 
 using namespace std;
 
 //"*&" -> alias for passing parameter (allows modification)
+string to_string(int count);
 string bruteForce(string input);
 
+//Convert the frequencies of characters to string
+string to_string(int count)
+{
+	int length = snprintf(NULL, 0, "%d", count);
+
+	char * buffer = new char[length + 1];
+	snprintf(buffer, length + 1, "%d", count);
+	string freq (buffer);
+
+	return freq;
+}
+
+/*
+Time Complexity: O(n)
+Only goes through string once
+
+Space Complexity: O(n)
+Requires extra string for returning the data
+*/
 string bruteForce(string input)
 {
+	int len = input.length();
 
+	//Any size less than 2 means that the result str > input str
+	if (len < 2) return input;
+
+	string result = "";
+	int count = 1;
+
+	for (int i = 0; i < len; ++i)
+	{
+		//Look-ahead method
+		if ((i + 1) < len)
+		{
+			//Count until a different letter is reached
+			if (input[i] == input[i + 1])
+			{
+				++count;
+			}
+			else
+			{
+				//Convert the count to string
+				string freq = to_string(count);
+				result += input[i] + freq;
+				count = 1;	
+			}
+		}
+		else
+		{
+			string freq = to_string(count);
+			result += input[i] + freq;
+			if (result.length() >= len) return input;
+		}
+	}
+
+	return result;
 }
 
 int main()
 {
 	/*Should return a2b1c5a3*/
-	string s1 = "aabcccccaaa";
+	string s1 ("aabcccccaaa");
 
 	/*Should return abcd*/
-	string s2 = "abcd";
+	string s2 ("abcd");
 
-	/*Should return abca3b3*/
-	string s3 = "abcaaabbb";
+	/*Should return a4b3*/
+	string s3 ("aaaabbb");
+	
+	string r1 = bruteForce(s1);
+	string r2 = bruteForce(s2);
+	string r3 = bruteForce(s3);
 
-	bruteForce(s1);
+	cout << "(B) Result: " << r1 << endl;
+	cout << "(B) Result: " << r2 << endl;
+	cout << "(B) Result: " << r3 << endl;
 
 	return 0;
 }
