487db39250a8cc2223dee6fb5c5fc63e34b0d77b
dittoPDX <steventran325@gmail.com>
2017-09-20 00:14:18 -0700

    Added 3 matrix tests

--- CH1/1-7.cpp
+++ CH1/1-7.cpp
@@ -18,11 +18,32 @@ could you do this? Can you then extend it to four arrays?
 */
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main()
 {
+	int matrix[2][2] =
+	{
+		{1, 2},
+		{3, 4}
+	};
+
+	int matrix2[3][3] =
+	{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9}
+	};
+
+	int matrix3[4][4] =
+	{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
+		{13, 14, 15, 16}
+	};
 
 	return 0;
 }
