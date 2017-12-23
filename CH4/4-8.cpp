3f119652c3488b9a03645dc8ec9a6201826b85dc
dittoPDX <steventran325@gmail.com>
2017-12-23 13:50:31 -0800

    Updpdate for questions

---
+++ CH4/4-8.cpp
@@ -0,0 +1,7 @@
+/*
+- First Common Ancestor -
+Design an algorithm and write code to find
+the first common ancestor of 2 nodes in a 
+binary tree. Avoid storing additional nodes
+in a data structure.
+*/
---
+++ CH4/4-9.cpp
@@ -0,0 +1,7 @@
+/*
+- BST Sequences -
+A binary search tree was created by traversing through an array
+left to right and inserting each element. Given a BST with
+distinct elements, print all possible arrays that could have
+led to this tree.
+*/
